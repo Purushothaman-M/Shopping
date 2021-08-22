e8cb927a0c75b646ebc6b02cc0986439cfd65f5f
Purushothaman M <84196356+Purushothaman-M@users.noreply.github.com>
2021-08-22 11:00:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import Tabs from './component/tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+<Tabs/>
+</NavigationContainer>
+  )}
+export default App;
---
+++ sample.java
@@ -0,0 +1,6 @@
+Public class sample{
+
+public static void main(String args[]){
+	System.out.println("Hello world");
+}
+}
